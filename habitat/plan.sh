223343604412b4eb31e5b826ec5f06c52b44fbf1
Greg Fodor <gfodor@gmail.com>
2018-06-27 16:33:19 +0000

    Fix for build with ghostscript

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -9,9 +9,20 @@ pkg_license=("MIT")
 pkg_source="https://github.com/gfodor/farspark"
 pkg_bin_dirs=(bin)
 pkg_deps=(core/glibc core/gcc-libs core/bash mozillareality/ghostscript)
+pkg_build_deps=(mozillareality/ghostscript)
 pkg_scaffolding=core/scaffolding-go
 scaffolding_go_base_path=github.com/gfodor/farspark
 scaffolding_go_build_deps=()
+
+do_download() {
+  # HACK: need to set CGO environment here since the download stage fails otherwise
+
+  _build_environment
+  export CGO_CFLAGS=$CFLAGS
+  export CGO_LDFLAGS=$LDFLAGS
+
+  do_default_download
+}
 
 do_build() {
   do_default_build
