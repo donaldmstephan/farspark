5bcd4f3202322a409fadd330d06db1d68867b131
Marshall Quander <marshall@quander.me>
2018-08-24 15:10:04 -0700

    Fix up things that say gfodor to say MozillaReality

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,17 +1,17 @@
 pkg_name=farspark
 pkg_description="Fast and secure standalone proxy server for serving, resizing, and converting remote media"
-pkg_upstream_url="https://github.com/gfodor/farspark"
+pkg_upstream_url="https://github.com/MozillaReality/farspark"
 pkg_origin=mozillareality
 pkg_version="v1.3"
 pkg_maintainer=''
 pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
 pkg_license=("MIT")
-pkg_source="https://github.com/gfodor/farspark"
+pkg_source="https://github.com/MozillaReality/farspark"
 pkg_bin_dirs=(bin)
 pkg_deps=(core/glibc core/gcc-libs core/bash mozillareality/ghostscript)
 pkg_build_deps=(mozillareality/ghostscript)
 pkg_scaffolding=core/scaffolding-go
-scaffolding_go_base_path=github.com/gfodor/farspark
+scaffolding_go_base_path=github.com/MozillaReality/farspark
 scaffolding_go_build_deps=()
 
 do_download() {
@@ -33,9 +33,9 @@ do_install() {
   mkdir -p "$pkg_prefix/include"
   mkdir -p "$pkg_prefix/share"
 
-  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/gfodor/farspark/vendor/github.com/discordapp/lilliput/deps/linux/lib" "$pkg_prefix"
-  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/gfodor/farspark/vendor/github.com/discordapp/lilliput/deps/linux/include" "$pkg_prefix"
-  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/gfodor/farspark/vendor/github.com/discordapp/lilliput/deps/linux/share" "$pkg_prefix"
+  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/MozillaReality/farspark/vendor/github.com/discordapp/lilliput/deps/linux/lib" "$pkg_prefix"
+  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/MozillaReality/farspark/vendor/github.com/discordapp/lilliput/deps/linux/include" "$pkg_prefix"
+  cp -r "$HAB_CACHE_SRC_PATH/scaffolding-go-gopath/src/github.com/MozillaReality/farspark/vendor/github.com/discordapp/lilliput/deps/linux/share" "$pkg_prefix"
 
   do_default_install
 }
